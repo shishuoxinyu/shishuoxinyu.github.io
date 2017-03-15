1fb9ea9f1487be52dcd0e778418f0f0a7bd5d809
enilu <sheseerzuo123>
2017-03-15 13:01:58 +0800

    add 多说

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//百度站长工具
 (function(){
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
@@ -10,4 +10,16 @@
     }
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
+})();
+
+
+//多说评论
+var duoshuoQuery = {short_name:"instagramfans"};
+(function() {
+    var ds = document.createElement('script');
+    ds.type = 'text/javascript';ds.async = true;
+    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+    ds.charset = 'UTF-8';
+    (document.getElementsByTagName('head')[0]
+    || document.getElementsByTagName('body')[0]).appendChild(ds);
 })();
